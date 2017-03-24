4c13d007da89d714f1968aa3d8ee7c3cb2d4766b
lunqw <kaaron@163.com>
2017-03-24 13:05:19 +0800

    PVManager增加一个方法bind(final View view)

--- pvm/src/main/java/com/yy/lqw/pvm/PVManager.java
+++ pvm/src/main/java/com/yy/lqw/pvm/PVManager.java
@@ -17,30 +17,43 @@ public enum PVManager {
     private static final String TAG = "PVManager";
 
     /**
-     * @param viewObject      含有PVM注解的任意对象，可以是Activity、Fragment、View，
+     * 绑定View和Present对象
+     *
+     * @param view MVP中的view对象, 必须含有PVM注解{@link PVM}
+     * @return Presenter对象，生存周期跟view一致
+     */
+    public Presenter bind(final View view) {
+        return bind(view, view);
+    }
+
+    /**
+     * 绑定View和Present对象
+     *
+     * @param viewObject      MVP中的view对象, 必须含有PVM注解{@link PVM}，可以是Activity、Fragment、View，
      *                        又或者是用于单元测试的Mock对象
-     * @param lifeCycleObject View对象，用于控制presenter的生命周期
-     * @return presenter Presenter对象
+     * @param lifeCycleObject lifeCycleObject对象，用于控制presenter的生命周期
+     * @return Presenter对象，生存周期跟lifeCycleObject一致
      */
     public Presenter bind(final Object viewObject, final View lifeCycleObject) {
         if (viewObject == null || lifeCycleObject == null) {
             throw new NullPointerException("viewObject or view was null");
         }
-        Class<?> viewClass = viewObject.getClass();
-        PVM pvmAnnotation = viewClass.getAnnotation(PVM.class);
+
+        Presenter result = null;
+        final Class<?> viewClass = viewObject.getClass();
+        final PVM pvmAnnotation = viewClass.getAnnotation(PVM.class);
         if (pvmAnnotation == null) {
             throw new IllegalArgumentException("@PVM annotation miss");
         }
-        Presenter result = null;
-        Class<? extends Presenter> presenterClass = pvmAnnotation.presenter();
+        final Class<? extends Presenter> presenterClass = pvmAnnotation.presenter();
         try {
             final Presenter presenter = (result = presenterClass.newInstance());
             final String delegateImplName = viewClass.getName()
                     + presenterClass.getSimpleName()
                     + "DelegateImpl";
-            Class<? extends Delegate> delegateClass = Class.forName(delegateImplName)
+            final Class<? extends Delegate> delegateClass = Class.forName(delegateImplName)
                     .asSubclass(Delegate.class);
-            Constructor<? extends Delegate> constructor = delegateClass.getConstructor(viewClass);
+            final Constructor<? extends Delegate> constructor = delegateClass.getConstructor(viewClass);
             final Delegate delegate = constructor.newInstance(viewObject);
             lifeCycleObject.addOnAttachStateChangeListener(new View.OnAttachStateChangeListener() {
                 @Override
